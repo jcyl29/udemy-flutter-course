0f2e624cd35ad708029b942a2a7600652819c6a5
Justin Lui <jlui@surveymonkey.com>
2019-08-25 13:34:16 -0700

    expense tracker app progress

---
+++ expense_tracker/lib/main.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+import './widgets/user_transactions.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter App',
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  // String titleInput;
+  // String amountInput;
+
+  final titleController = TextEditingController();
+  final amountController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Flutter App'),
+      ),
+      body: Column(
+          // mainAxisAlignment: MainAxisAlignment.spaceAround,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            // Column only takes as much width as its child elements need
+            Container(
+              width: double.infinity,
+              child: Card(
+                color: Colors.blue,
+                child: Text('CHART'),
+                elevation: 5,
+              ),
+            ),
+            UserTransactions(),
+          ]),
+    );
+  }
+}
